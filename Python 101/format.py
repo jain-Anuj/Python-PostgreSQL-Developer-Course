9642ef878bc5f83f38bc01dbc5ee5665dad5fbbb
jain-Anuj <jainanuj9415@gmail.com>
2019-06-13 21:28:33 +0530

    Udemy First Commit

---
+++ Python 101/format.py
@@ -0,0 +1,2 @@
+age = int(input("Enter your age: "))
+print("You have lived for {} seconds. And your age is {}".format(str(age*365*24*60*60),age))
---
+++ Python 101/input.py
@@ -0,0 +1,2 @@
+age = input("Enter your age: ")
+print("You have lived for "+str(age*365*24*60*60)+" seconds. This corresponds to "+ str(age) + " years.")
