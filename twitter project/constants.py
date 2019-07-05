b464c8ca828eed9b9dcb8b22ffc1a38aee8b6ebb
jain-Anuj <jainanuj9415@gmail.com>
2019-07-05 10:31:02 +0530

    copied constant file to local

--- twitter project/constants.py
+++
@@ -1,6 +0,0 @@
-CONSUMER_KEY= '0eSZCDRs6Gmj3k0HFw3VhYkV5'
-CONSUMER_SECRET = 'YGwwbK6aW6sre0KySfLYrDg0j9DWdTpjKVuWc68NYLGFukr3QN'
-
-REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
-ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
-AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
